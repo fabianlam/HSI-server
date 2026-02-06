ce5276b5728b1f18cc53c607286b947a93001415
fabianlam <fabianlam1978@yahoo.com.hk>
2026-02-06 01:52:04 +0000

    Update hsi_server.py

--- hsi_server.py
+++ hsi_server.py
@@ -45,7 +45,7 @@ def advertise_mdns():
     zeroconf.register_service(info)
     try:
         while True:
-            time.sleep(2)
+            time.sleep(10)
     except KeyboardInterrupt:
         print("Unregistering mDNS service...")
         zeroconf.unregister_service(info)
